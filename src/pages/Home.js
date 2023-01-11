cc9ae6cf6a366e4bb934e99e8d6cc016bb25626a
unknown <grady.andre2@gmail.com>
2023-01-11 11:48:56 -0800

    fixing height of text on homepage.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,48 @@
-import { Stack, Typography,Divider } from "@mui/material";
+import { Stack, Typography, Divider, IconButton, Link, Container } from "@mui/material";
 import TypewriterComponent from "typewriter-effect";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 export default function Home() {
   return (
+    <Container maxWidth='md'>
+
     <section className="home">
       <Typography variant="h1" color={"primary.main"}>
         <TypewriterComponent
           options={{
-            strings: ["Andre' Grady"],
-            autoStart: true,
-            loop: true,
-          }}
-        />
+              strings: ["Andre' Grady"],
+              autoStart: true,
+              loop: true,
+            }}
+            />
       </Typography>
       <Typography variant="h3">Front-end Web-Developer</Typography>
-      <Stack 
-      direction="row" 
-      spacing={8} 
-      divider={<Divider orientation="vertical" color="black" flexItem />}
-      >
-        <GitHubIcon sx={{fontSize:'50px'}} />
-        <LinkedInIcon sx={{fontSize:'50px'}} />
+      <Stack
+        direction="row"
+        spacing={8}
+        alignItems='center'
+        mt={6}
+        divider={<Divider orientation="vertical" color="black" flexItem />}
+        >
+        <IconButton sx={{ color: "black" }}>
+          <Link 
+            href="https://github.com/Grady253" 
+            color="inherit"
+            >
+            <GitHubIcon sx={{ fontSize: "50px" }} />
+          </Link>
+        </IconButton>
+        <IconButton sx={{ color: "black" }}>
+          <Link
+            href="https://www.linkedin.com/in/andre-grady-a70237190/"
+            color="inherit"
+            >
+            <LinkedInIcon sx={{ fontSize: "50px" }} />
+          </Link>
+        </IconButton>
       </Stack>
     </section>
+              </Container>
   );
 }
