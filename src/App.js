b01f5865037ecb22e5ec0de9b11a5074fc44251a
unknown <grady.andre2@gmail.com>
2023-01-11 11:10:45 -0800

    added a couple icons to home page.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,28 @@
-import { ThemeProvider } from '@emotion/react';
-import { Container, Paper, createTheme } from '@mui/material';
-import background from './photos/background.jpg';
-import './App.css';
-import Home from './pages/Home';
+import { ThemeProvider } from "@emotion/react";
+import { Container, createTheme } from "@mui/material";
+// import background from './photos/background.jpg';
+import "./App.css";
+import Home from "./pages/Home";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main:'#F2AF29',
+      main: "#F2AF29",
     },
-    secondary:{
-      main:'#7E7F83',
+    secondary: {
+      main: "#7E7F83",
     },
   },
 });
 
-const style = {
-  paperContainer:{
-    backgroundImage: `url(${background})`,
-    backgroundSize:'cover',
-  }
-}
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Paper style={style.paperContainer}>
+      <div className="App">
         <Container maxWidth>
-          <div className='App'>
-            <Home/>
-          </div>
+          <Home />
         </Container>
-      </Paper>
+      </div>
     </ThemeProvider>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,29 @@
-import { Typography } from "@mui/material";
+import { Stack, Typography,Divider } from "@mui/material";
 import TypewriterComponent from "typewriter-effect";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
-export default function Home(){
-    return (
-        <section>
-          <Typography variant="h1" color={"primary.main"}>
-             <TypewriterComponent 
-             options={{
-                strings: ["Andre' Grady"],
-                autoStart: true,
-                loop: true,
-             }}
-             />
-             </Typography>
-             <Typography variant="h3">
-             Front-end Web-Developer
-             </Typography>
-        </section>
-    )
+export default function Home() {
+  return (
+    <section className="home">
+      <Typography variant="h1" color={"primary.main"}>
+        <TypewriterComponent
+          options={{
+            strings: ["Andre' Grady"],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </Typography>
+      <Typography variant="h3">Front-end Web-Developer</Typography>
+      <Stack 
+      direction="row" 
+      spacing={8} 
+      divider={<Divider orientation="vertical" color="black" flexItem />}
+      >
+        <GitHubIcon sx={{fontSize:'50px'}} />
+        <LinkedInIcon sx={{fontSize:'50px'}} />
+      </Stack>
+    </section>
+  );
 }
