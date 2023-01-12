b6f20141cff45de0fcdd0a0f19ec55e624d4d16d
unknown <grady.andre2@gmail.com>
2023-01-12 09:59:29 -0800

    fixing icons placement.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,48 +1,53 @@
-import { Stack, Typography, Divider, IconButton, Link, Container } from "@mui/material";
+import {
+  Stack,
+  Typography,
+  Divider,
+  IconButton,
+  Link,
+  Container,
+} from "@mui/material";
 import TypewriterComponent from "typewriter-effect";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 export default function Home() {
   return (
-    <Container maxWidth='md'>
-
-    <section className="home">
-      <Typography variant="h1" color={"primary.main"}>
-        <TypewriterComponent
-          options={{
+    <Container maxWidth="md">
+      <section className="home">
+        <Typography variant="h1" color={"primary.main"}>
+          <TypewriterComponent
+            options={{
               strings: ["Andre' Grady"],
               autoStart: true,
               loop: true,
             }}
-            />
-      </Typography>
-      <Typography variant="h3">Front-end Web-Developer</Typography>
-      <Stack
-        direction="row"
-        spacing={8}
-        alignItems='center'
-        mt={6}
-        divider={<Divider orientation="vertical" color="black" flexItem />}
-        >
-        <IconButton sx={{ color: "black" }}>
-          <Link 
-            href="https://github.com/Grady253" 
-            color="inherit"
-            >
-            <GitHubIcon sx={{ fontSize: "50px" }} />
-          </Link>
-        </IconButton>
-        <IconButton sx={{ color: "black" }}>
-          <Link
-            href="https://www.linkedin.com/in/andre-grady-a70237190/"
-            color="inherit"
-            >
-            <LinkedInIcon sx={{ fontSize: "50px" }} />
-          </Link>
-        </IconButton>
-      </Stack>
-    </section>
-              </Container>
+          />
+        </Typography>
+        <Typography variant="h3">Front-end Web-Developer</Typography>
+        <div className="icon-place">
+
+        <Stack
+          direction="row"
+          spacing={4}
+          mt={6}
+          divider={<Divider orientation="vertical" color="black" flexItem />}
+          >
+          <IconButton sx={{ color: "black" }}>
+            <Link href="https://github.com/Grady253" color="inherit">
+              <GitHubIcon sx={{ fontSize: "50px" }} />
+            </Link>
+          </IconButton>
+          <IconButton sx={{ color: "black" }}>
+            <Link
+              href="https://www.linkedin.com/in/andre-grady-a70237190/"
+              color="inherit"
+              >
+              <LinkedInIcon sx={{ fontSize: "50px" }} />
+            </Link>
+          </IconButton>
+        </Stack>
+              </div>
+      </section>
+    </Container>
   );
 }
