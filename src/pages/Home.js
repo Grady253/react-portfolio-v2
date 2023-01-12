2b945a632969863bb07b14bf60a943fc9d30b008
unknown <grady.andre2@gmail.com>
2023-01-12 10:29:16 -0800

    added resume button.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import {
   IconButton,
   Link,
   Container,
+  Button,
 } from "@mui/material";
 import TypewriterComponent from "typewriter-effect";
 import GitHubIcon from "@mui/icons-material/GitHub";
@@ -14,7 +15,7 @@ export default function Home() {
   return (
     <Container maxWidth="md">
       <section className="home">
-        <Typography variant="h1" color={"primary.main"}>
+        <Typography sx={{fontSize:"130px"}} color={"primary.main"}>
           <TypewriterComponent
             options={{
               strings: ["Andre' Grady"],
@@ -23,30 +24,32 @@ export default function Home() {
             }}
           />
         </Typography>
-        <Typography variant="h3">Front-end Web-Developer</Typography>
+        <Typography sx={{fontSize:"50px"}}>Software Engineer</Typography>
         <div className="icon-place">
-
-        <Stack
-          direction="row"
-          spacing={4}
-          mt={6}
-          divider={<Divider orientation="vertical" color="black" flexItem />}
+          <Stack
+            direction="row"
+            spacing={4}
+            mt={6}
+            divider={<Divider orientation="vertical" color="black" flexItem />}
           >
-          <IconButton sx={{ color: "black" }}>
-            <Link href="https://github.com/Grady253" color="inherit">
-              <GitHubIcon sx={{ fontSize: "50px" }} />
-            </Link>
-          </IconButton>
-          <IconButton sx={{ color: "black" }}>
-            <Link
-              href="https://www.linkedin.com/in/andre-grady-a70237190/"
-              color="inherit"
+            <IconButton sx={{ color: "black" }}>
+              <Link href="https://github.com/Grady253" sx={{color:"#171515"}}>
+                <GitHubIcon sx={{ fontSize: "40px" }} />
+              </Link>
+            </IconButton>
+            <IconButton sx={{ color: "black" }}>
+              <Link
+                href="https://www.linkedin.com/in/andre-grady-a70237190/"
+                sx={{color:"#0A66C2"}}
               >
-              <LinkedInIcon sx={{ fontSize: "50px" }} />
-            </Link>
-          </IconButton>
-        </Stack>
-              </div>
+                <LinkedInIcon sx={{ fontSize: "40px" }} />
+              </Link>
+            </IconButton>
+            <Button variant="contained">
+              Resume
+            </Button>
+          </Stack>
+        </div>
       </section>
     </Container>
   );
