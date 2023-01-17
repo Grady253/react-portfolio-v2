dff16bd0f7555ec73bd93981e7e0b760b9ca9b7c
Grady253 <grady.andre2@gmail.com>
2023-01-16 21:34:10 -0800

    added hover effect to arrow.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,28 +1,27 @@
-import { Box, Typography, Container } from "@mui/material";
+import { Typography, Container } from "@mui/material";
 import Andre from "../photos/Andre.png";
 
 export default function About() {
   return (
-    <Container maxWidth="md">
-      <Typography variant="h2" color="white">
+    <Container maxWidth="lg">
+      <Typography variant="h2" color="white" sx={{mb:30, mt:20}}>
         About Me
       </Typography>
       <section className="about-Me">
-        <Box sx={{ width: 600, height: "auto" }}>
-          <Typography variant="h3" color="primary">
-            Hi there,
-          </Typography>
-          <Typography variant="h5" color="white" sx={{alignItems:'left'}}>
-            My name is Andre' Grady. I'm a software engineer who recently
+        <div id="about-Text">
+          <Typography variant="h4" color="white">
+            Hi there, my name is Andre' Grady. I'm a software engineer who recently
             graduated from the university of washington full-stack Coding
             Bootcamp. I am 24 years olds actively seeking an opportunity to
             showcase my talents and be an asset to any team willing to accept
             me.
           </Typography>
-          <Box sx={{ width: 100, height: 200 }}>
-            <img alt="Andre sitting down" src={Andre} id="andre" />
-          </Box>
-        </Box>
+        </div>
+        <div id="about-picture">
+          <img alt="Andre sitting down" src={Andre} id="andre" />
+        </div>
+        {/* <Box id='about-Pic' sx={{ width: 100, height: 200}}> */}
+        {/* </Box> */}
       </section>
     </Container>
   );
