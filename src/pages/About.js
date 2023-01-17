501e7c96db4cc0b111364f780a84b0dcf4b6aeef
Grady253 <grady.andre2@gmail.com>
2023-01-17 10:03:14 -0800

    added scroll effect on click of arrow and hover effect.

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,26 +3,27 @@ import Andre from "../photos/Andre.png";
 
 export default function About() {
   return (
-    <Container maxWidth="lg">
-      <Typography variant="h2" color="white" sx={{mb:30, mt:20}}>
-        About Me
-      </Typography>
-      <section className="about-Me">
-        <div id="about-Text">
-          <Typography variant="h4" color="white">
-            Hi there, my name is Andre' Grady. I'm a software engineer who recently
-            graduated from the university of washington full-stack Coding
-            Bootcamp. I am 24 years olds actively seeking an opportunity to
-            showcase my talents and be an asset to any team willing to accept
-            me.
-          </Typography>
-        </div>
-        <div id="about-picture">
-          <img alt="Andre sitting down" src={Andre} id="andre" />
-        </div>
-        {/* <Box id='about-Pic' sx={{ width: 100, height: 200}}> */}
-        {/* </Box> */}
-      </section>
+    <Container maxWidth="xl">
+      <div id="about">
+        <Typography variant="h2" color="white" sx={{ mb: 20, mt: 30 }}>
+          About Me
+        </Typography>
+        <section className="about-Me">
+          <div id="about-Text">
+            <Typography variant="h4" color="white">
+              Hi, my name is Andre' Grady. I'm a software engineer who recently
+              graduated from the university of washington full-stack Coding
+              Bootcamp. I am 24 years old actively seeking an opportunity to
+              showcase my talents and be an asset to any team willing to accept
+              me.
+            </Typography>
+          </div>
+          <div id="about-picture">
+            <img alt="Andre sitting down" src={Andre} id="andre" />
+          </div>
+
+        </section>
+      </div>
     </Container>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,32 +3,32 @@ import {
   Typography,
   Divider,
   IconButton,
-  Link,
+  // Link,
   Container,
   Button,
 } from "@mui/material";
+import { Link } from "react-scroll/modules";
 import TypewriterComponent from "typewriter-effect";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 export default function Home() {
-  const onButtonClick = () =>{
-    fetch('Resume.pdf').then(response => {
-      response.blob().then(blob =>{
+  const onButtonClick = () => {
+    fetch("Resume.pdf").then((response) => {
+      response.blob().then((blob) => {
         const pdfURL = window.URL.createObjectURL(blob);
-        let alink = document.createElement('a');
+        let alink = document.createElement("a");
         alink.href = pdfURL;
         alink.download = "Resume.pdf";
         alink.click();
-      })
-    })
+      });
+    });
   };
   return (
     <Container maxWidth="md">
       <section className="home">
-        <Typography sx={{fontSize:"130px"}} color={"primary.main"}>
+        <Typography sx={{ fontSize: "130px" }} color={"primary.main"}>
           <TypewriterComponent
             options={{
               strings: ["Andre' Grady"],
@@ -37,33 +37,51 @@ export default function Home() {
             }}
           />
         </Typography>
-        <Typography sx={{fontSize:"50px", color:'white'}}>Software Engineer</Typography>
-          <Stack
-            direction="row"
-            spacing={4}
-            mt={6}
-            divider={<Divider orientation="vertical" color="white" flexItem />}
-            style={{justifyContent:"center"}}
+        <Typography sx={{ fontSize: "50px", color: "white" }}>
+          Software Engineer
+        </Typography>
+        <Stack
+          direction="row"
+          spacing={4}
+          mt={6}
+          divider={<Divider orientation="vertical" color="white" flexItem />}
+          style={{ justifyContent: "center" }}
+        >
+          <IconButton sx={{ color: "black" }}>
+            <a href="https://github.com/Grady253" sx={{ color: "white" }}>
+              <GitHubIcon sx={{ fontSize: "40px" }} />
+            </a>
+          </IconButton>
+          <Button
+            sx={{ border: "solid 1px" }}
+            size="medium"
+            variant="contained"
+            onClick={onButtonClick}
           >
-            <IconButton sx={{ color: "black" }}>
-              <Link href="https://github.com/Grady253" sx={{color:"white"}}>
-                <GitHubIcon sx={{ fontSize: "40px" }} />
-              </Link>
-            </IconButton>
-            <Button sx={{border:"solid 1px"}} size="medium" variant="contained" onClick={onButtonClick}>
-              Resume
-            </Button>
-            <IconButton sx={{ color: "black" }}>
-              <Link
-                href="https://www.linkedin.com/in/andre-grady-a70237190/"
-                sx={{color:"#0A66C2"}}
-              >
-                <LinkedInIcon sx={{ fontSize: "40px" }} />
-              </Link>
-            </IconButton>
-          </Stack>
-          
-          <KeyboardArrowDownIcon sx={{fontSize:"90px", color:'white', justifyContent:'center', mt:'200px' }} id="arrow" />
+            Resume
+          </Button>
+          <IconButton sx={{ color: "black" }}>
+            <a
+              href="https://www.linkedin.com/in/andre-grady-a70237190/"
+              sx={{ color: "#0A66C2" }}
+            >
+              <LinkedInIcon sx={{ fontSize: "40px" }} />
+            </a>
+          </IconButton>
+        </Stack>
+        <IconButton>
+          <Link to="about" spy={true} smooth={true} offset={50} duration={500}>
+            <KeyboardArrowDownIcon
+              sx={{
+                fontSize: "90px",
+                color: "white",
+                justifyContent: "center",
+                mt: "200px",
+              }}
+              id="arrow"
+            />
+          </Link>
+        </IconButton>
       </section>
     </Container>
   );
