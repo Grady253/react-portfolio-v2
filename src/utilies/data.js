6f005d46caa6aebf77575d430e832145ae1af532
Grady253 <grady.andre2@gmail.com>
2023-01-20 10:56:08 -0800

    Added my project data.

---
+++ src/utilies/data.js
@@ -0,0 +1,35 @@
+import Planner from "../photos/DailyPlanner.png";
+import Music from "../photos/Music-Match.png";
+import Generator from "../photos/PasswordGenerator.png";
+
+const data = [
+  {
+    id: 1,
+    title: "Daily Planner",
+    description:
+      "This is an application that allows the user to plan out their daily task with a color coordination of time to keep them on track.",
+    image: Planner,
+    repository: "https://github.com/Grady253/daily_planner",
+    url: "https://grady253.github.io/daily_planner/",
+  },
+  {
+    id: 2,
+    title: "Password Generator",
+    description:
+      "This is an appllication that allows the user to generate a random password.",
+    image: Generator,
+    repository: "https://github.com/Grady253/generate-password",
+    url: "https://grady253.github.io/generate-password/",
+  },
+  {
+    id: 3,
+    title: "Music Match",
+    description:
+      "This is an app that built to allow musicians and artist make connections. ",
+    image: Music,
+    repository: "https://github.com/KarlOL82/music-match",
+    url: "https://groove-connection.herokuapp.com/",
+  },
+];
+
+export default data;
