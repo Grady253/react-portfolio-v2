d004901777bb55b999f00cc4d2bfc6040e6b18f5
Grady253 <grady.andre2@gmail.com>
2023-01-24 11:12:35 -0800

    got projects centered.

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -9,53 +9,71 @@ import {
   Grid,
   Box,
   CardActionArea,
+  Container,
 } from "@mui/material";
 import projectData from "../utilies/data";
 
 export default function Portfolio() {
-  
   return (
     <section id="portfolio">
-      <Typography
-        variant="h2"
-        color="white"
-        id="about-title"
-        sx={{ mb: 20, mt: 25, fontWeight: "bold" }}
-      >
-        Portfolio
-      </Typography>
-      <Box component='div' sx={{display:'flex',justifyContent:"center", alignItems:"center"}}>
-      <Grid container spacing={4} >
-        {projectData.map(({title, description, image, url})=>(
-          <Grid item xs={10} sm={6} md={4}>
-          <Card sx={{width:345}}>
-            <CardActionArea >
-            <CardMedia 
-              id='card-image'
-              component='img'
-              height='150'
-              src={image}
-              alt='Projects'
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5">
-                {title}
-              </Typography>
-              <Typography variant="body2" color='textSecondary' component='p'>
-                {description}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="medium" href={url} target='_blank'>
-                Live Demo
-              </Button>
-            </CardActions>
-            </CardActionArea>
-          </Card>
+      <Container maxWidth="lg">
+        <Typography
+          variant="h2"
+          color="white"
+          id="about-title"
+          sx={{ mb: 20, mt: 25, fontWeight: "bold" }}
+        >
+          Portfolio
+        </Typography>
+        <Box
+          component="div"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Grid container spacing={4}>
+            {projectData.map(({ title, description, image, url }) => (
+              <Grid item xs={10} sm={6} md={4}>
+                <Card sx={{ width: 345 }}>
+                  <CardActionArea>
+                    <CardMedia
+                      id="card-image"
+                      component="img"
+                      height="150"
+                      src={image}
+                      alt="Projects"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5">
+                        {title}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                      >
+                        {description}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button
+                        size="medium"
+                        variant="contained"
+                        href={url}
+                        target="_blank"
+                      >
+                        Live Demo
+                      </Button>
+                    </CardActions>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
-      </Box>
+        </Box>
+      </Container>
     </section>
   );
 }
