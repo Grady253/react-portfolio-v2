c256c32fda7e7b1e276a21f308088250ddd70424
Grady253 <grady.andre2@gmail.com>
2023-01-23 21:12:51 -0800

    changed background color.

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,39 +1,93 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Container, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+  Grid,
+  Box,
+} from "@mui/material";
 import projectData from "../utilies/data";
 
 export default function Portfolio() {
   return (
-    <Container maxWidth="xl">
-      <div id="portfolio">
-        <Typography
-          variant="h2"
-          color="white"
-          id="about-title"
-          sx={{ mb: 20, mt: 25, fontWeight: "bold" }}
+    <section id="portfolio">
+      <Typography
+        variant="h2"
+        color="white"
+        id="about-title"
+        sx={{ mb: 20, mt: 25, fontWeight: "bold" }}
+      >
+        Portfolio
+      </Typography>
+      <Box sx={{ flexGrow: 1, padding: 5 }}>
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          justifyContent="center"
+          align="center"
         >
-          Portfolio
-        </Typography>
-          {projectData.map(({title, description, image, respository, url})=>(
-        <div className="cards">
-            <Card sx={{maxWidth:600, height:400, borderRadius:10,border:"9px solid white", position:'relative'}}>
-              <CardMedia
-                id='card-image'
-                component='img'
-                image={image}
-                alt=''
-              />
-              <CardContent className="card-body">
-                <Typography className="card-title" variant="h3" component='div'>{title}</Typography>
-                <Typography className="card-info"  variant='h6' component='p'>{description}</Typography>
-                <CardActions className="card-btn">
-                  <Button  variant="contained" size='large' href={respository} target='_blank'>Repo</Button>
-                  <Button variant="contained" size='large' href={url} target='_blank'>Live</Button>
-                </CardActions>
-              </CardContent>
-            </Card>
-        </div>
-          ))}
-      </div>
-    </Container>
+          {projectData.map(
+            ({ title, description, image, respository, url }) => (
+              <Grid item xs={2} sm={4} md={4}>
+                <Card
+                  sx={{
+                    height: 550,
+                    width: 400,
+                    borderRadius: 10,
+                    border: "9px solid primary",
+                  }}
+                >
+                  <CardMedia
+                    id="card-image"
+                    component="img"
+                    image={image}
+                    alt=""
+                  />
+                  <div className="content">
+                    <CardContent>
+                      <Typography
+                        className="card-title"
+                        variant="h3"
+                        component="div"
+                      >
+                        {title}
+                      </Typography>
+                      <Typography
+                        className="card-info"
+                        variant="h6"
+                        component="p"
+                      >
+                        {description}
+                      </Typography>
+                      <CardActions className="card-btn">
+                        <Button
+                          variant="contained"
+                          size="large"
+                          href={respository}
+                          target="_blank"
+                        >
+                          Repo
+                        </Button>
+                        <Button
+                          variant="contained"
+                          size="large"
+                          href={url}
+                          target="_blank"
+                        >
+                          Live
+                        </Button>
+                      </CardActions>
+                    </CardContent>
+                  </div>
+                </Card>
+              </Grid>
+            )
+          )}
+        </Grid>
+      </Box>
+    </section>
   );
 }
